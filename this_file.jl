7e30be6037ee5c951d662c7b6271695a7564b964
Araz <71676938+arazn845@users.noreply.github.com>
2022-09-18 18:36:31 +1000

    Update this_file.jl

--- this_file.jl
+++ this_file.jl
@@ -1,11 +1,1 @@
-### A Pluto.jl notebook ###
-# v0.19.9
-
-using Markdown
-using InteractiveUtils
-
-# ╔═╡ c5784b70-372b-11ed-1af7-29dd40eb8dcb
 println("this is araZ")
-
-# ╔═╡ Cell order:
-# ╠═c5784b70-372b-11ed-1af7-29dd40eb8dcb
